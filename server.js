8eec5ed6a4fbebf1db4a8add088349520fe5a54d
AustinMaddox <austin@maddoxbox.com>
2018-10-08 10:47:39 -0600

    Remove publishing to Redis
    This isn't really needed and is causing a recursive loop in logic. We would only ever need to publish to Redis if we decided to use a PHP/Redis subscriber.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let server = require('http').createServer();
 let io = require('socket.io')(server);
 
 let redis = require('redis');
-let redis_pub_client = redis.createClient('redis://redis-4-0:6379');
+// let redis_pub_client = redis.createClient('redis://redis-4-0:6379');
 let redis_sub_client = redis.createClient('redis://redis-4-0:6379');
 
 const CHANNEL = 'test-channel';
@@ -18,6 +18,7 @@ redis_sub_client.subscribe(CHANNEL);
 redis_sub_client.on('message', function (channel, message) {
     console.log('Redis subscriber received a message on channel `%s` channel that says `%s`.', channel, message);
     io.sockets.emit(channel, message);
+    console.log('Redis subscriber emitted the message to the `%s` channel that says `%s`.', channel, message);
 });
 
 // Socket.io
@@ -25,7 +26,9 @@ io.on('connection', function (socket) {
     console.log('A socket.io client connected at ' + Date());
     socket.on(CHANNEL, function (message) {
         console.log('A socket.io message was received that says `%s`', message);
-        redis_pub_client.publish(CHANNEL, message);
-        console.log('A socket.io message was published to Redis that says `%s`', message);
+        io.sockets.emit(CHANNEL, message);
+        console.log('A socket.io message was emitted that says `%s`', message);
+        // redis_pub_client.publish(CHANNEL, message);
+        // console.log('A socket.io message was published to Redis that says `%s`', message);
     });
 });
